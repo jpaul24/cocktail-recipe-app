099ad897e156d119a92aa4c0f2bc112c4e4c3713
Joe Paul <joe.paul0712@gmail.com>
2020-02-19 10:18:39 -0800

    adding the controllers

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,3 +1,16 @@
 class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktails = Cocktail.new
+  end
+
 
 end
