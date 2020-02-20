c938a30ee0f366dc917a32eda03cfc32450e4414
Joe Paul <joe.paul0712@gmail.com>
2020-02-20 14:22:00 -0800

    finishing the models

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,7 @@
 class Cocktail < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+  mount_uploader :photo, PhotoUploader
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,5 @@
 class Dose < ApplicationRecord
+  validates :cocktail, uniqueness: { scope: :ingredient }
   belongs_to :cocktail
   belongs_to :ingredient
 end
